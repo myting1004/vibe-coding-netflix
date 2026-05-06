e5e34eee8749d1f77b51aba73fcadcdf1c8c9507
김태형 <taehyoung.kim@nts-corp.com>
2026-05-06 20:18:29 +0900

    config.js 일시적으로 .gitignore 해제

---
+++ config.js
@@ -0,0 +1,9 @@
+// TMDB API 설정
+// 주의: 이 파일은 .gitignore 에 등록되어 있다. 공개 저장소에 절대 커밋하지 않는다.
+const CONFIG = {
+    API_KEY: '95bfb59ffb4046ae718e5363e03f0f21',
+    BASE_URL: 'https://api.themoviedb.org/3',
+    IMAGE_BASE_URL: 'https://image.tmdb.org/t/p',
+    LANGUAGE: 'ko-KR',
+    REGION: 'KR'
+};
